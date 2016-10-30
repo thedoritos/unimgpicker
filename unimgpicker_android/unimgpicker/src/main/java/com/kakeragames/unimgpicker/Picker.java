1a66d8f98835ff11be50eaa6466b725a859893bc
thedoritos <thedoritos@gmail.com>
2016-10-30 16:33:48 +0900

    Open picker

--- unimgpicker_android/unimgpicker/src/main/java/com/kakeragames/unimgpicker/Picker.java
+++ unimgpicker_android/unimgpicker/src/main/java/com/kakeragames/unimgpicker/Picker.java
@@ -1,6 +1,54 @@
 package com.kakeragames.unimgpicker;
 
-public class Picker
-{
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.net.Uri;
 
+import com.unity3d.player.UnityPlayer;
+
+public class Picker extends Fragment
+{
+	private static final String TAG = "unimgpicker";
+	private static final int REQUEST_CODE = 1;
+
+	private static final String CALLBACK_OBJECT = "Unimgpicker";
+	private static final String CALLBACK_METHOD = "OnComplete";
+
+	public static void show(String title) {
+		Activity unityActivity = UnityPlayer.currentActivity;
+		if (unityActivity == null) {
+			// TODO: Notify failure.
+			return;
+		}
+
+		Fragment fragment = new Fragment();
+		FragmentTransaction transaction = unityActivity.getFragmentManager().beginTransaction();
+
+		transaction.add(fragment, TAG);
+		transaction.commit();
+
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+		intent.setType("image/*");
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		fragment.startActivityForResult(Intent.createChooser(intent, title), REQUEST_CODE);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode != REQUEST_CODE) {
+			return;
+		}
+
+		FragmentTransaction transaction = getActivity().getFragmentManager().beginTransaction();
+		transaction.remove(this);
+		transaction.commit();
+
+		Uri uri = data.getData();
+		UnityPlayer.UnitySendMessage(CALLBACK_OBJECT, CALLBACK_METHOD, uri.toString());
+	}
 }
