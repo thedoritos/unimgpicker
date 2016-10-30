5ad6e2e1c6e7206a5a0a661b7a6be7038d1fd6b5
thedoritos <thedoritos@gmail.com>
2016-10-30 17:00:43 +0900

    Add sample

---
+++ Assets/Unimgpicker/Scripts/Unimgpicker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kakera
+{
+    public class Unimgpicker : MonoBehaviour
+    {
+        private static readonly string PickerClass = "com.kakeragames.unimgpicker.Picker";
+
+        public void Show()
+        {
+            using (var picker = new AndroidJavaClass(PickerClass))
+            {
+                picker.CallStatic("show", "Select Image");
+            }
+        }
+
+        public void OnComplete(string url)
+        {
+            Debug.Log("Unimgpicker.OnComplete url:" + url);
+        }
+    }
+}
