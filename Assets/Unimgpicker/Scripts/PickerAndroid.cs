4523322cb7f251d18930a1b821a7829e33afe9ec
thedoritos <thedoritos@gmail.com>
2016-11-19 16:57:46 +0900

    Bridge iOS/Android

---
+++ Assets/Unimgpicker/Scripts/PickerAndroid.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_ANDROID
+using UnityEngine;
+
+namespace Kakera
+{
+    internal class PickerAndroid : IPicker
+    {
+        private static readonly string PickerClass = "com.kakeragames.unimgpicker.Picker";
+
+        public void Show(string title, string outputFileName)
+        {
+            using (var picker = new AndroidJavaClass(PickerClass))
+            {
+                picker.CallStatic("show", title, outputFileName);
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/Unimgpicker/Scripts/PickerUnsupported.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kakera
+{
+    internal class PickerUnsupported : IPicker
+    {
+        public void Show(string title, string outputFileName)
+        {
+            var message = "Unimgpicker is not supported on this platform.";
+            Debug.LogError(message);
+
+            var receiver = GameObject.Find("Unimgpicker");
+            if (receiver != null)
+            {
+                receiver.SendMessage("OnFailure", message);
+            }
+        }
+    }
+}
