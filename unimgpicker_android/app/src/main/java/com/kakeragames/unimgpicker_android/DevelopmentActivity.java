2dc64059ddbcafa456910bd5cfbb1f61945bc7d1
thedoritos <thedoritos@gmail.com>
2017-10-09 20:35:28 +0900

    Show image on debug app

--- unimgpicker_android/app/src/main/java/com/kakeragames/unimgpicker_android/DevelopmentActivity.java
+++ unimgpicker_android/app/src/main/java/com/kakeragames/unimgpicker_android/DevelopmentActivity.java
@@ -1,12 +1,19 @@
 package com.kakeragames.unimgpicker_android;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.kakeragames.unimgpicker.Picker;
 import com.unity3d.player.UnityPlayer;
+
+import java.io.File;
 
 public class DevelopmentActivity extends Activity {
 	@Override
@@ -16,11 +23,29 @@ public class DevelopmentActivity extends Activity {
 
 		UnityPlayer.currentActivity = this;
 
-		Button button = (Button) findViewById(R.id.button);
+		Button button = (Button) findViewById(R.id.button_open);
 		button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Picker.show("Development", "development", 512);
+				Picker.show("Development", "development", 1024);
+			}
+		});
+
+		Button reload = (Button) findViewById(R.id.button_reload);
+		reload.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Context context = DevelopmentActivity.this;
+
+				File image = context.getFileStreamPath("development");
+				if (!image.exists()) {
+					Toast.makeText(context, "Pick an image first.", Toast.LENGTH_SHORT).show();
+					return;
+				}
+
+				Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath());
+				ImageView imageView = (ImageView) findViewById(R.id.image_view);
+				imageView.setImageBitmap(bitmap);
 			}
 		});
 	}
