596c6f40d499e15e70a799be8c42fef219effb91
thedoritos <thedoritos@gmail.com>
2024-01-04 14:05:25 +0900

    Move back Samples into Assets
    - To make it easier to develop package in one Unity project

---
+++ Samples/SimplePickerSample/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Kakera
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 rotationVector;
+
+        void Update()
+        {
+            transform.Rotate(rotationVector * Time.deltaTime);
+        }
+    }
+}
